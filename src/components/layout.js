981ffcc13222b1140efdf599d509a753ad5a6bc9
Edward J <edjeong99@gmail.com>
2019-02-23 15:45:22 -0500

    add navbar and other needed components

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,21 @@
 import React from 'react'
 import '../assets/scss/main.scss'
+import Navbar from './Navbar'
 
 import Header from './Header'
 
 class Template extends React.Component {
-    render() {
-        const { children } = this.props
+  render() {
+    const { children } = this.props
 
-        return (
-            <div>
-                <Header />
-                {children}
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Header />
+        <Navbar />
+        {children}
+      </div>
+    )
+  }
 }
 
 export default Template
